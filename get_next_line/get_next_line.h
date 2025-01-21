9ed25a237b53e3350f85692a96e94ce178ae185b
Francisco Manuel <franciscomanueltoledo@gmail.com>
2025-01-21 15:43:29 +0100

    get next line

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -14,13 +14,7 @@
 # define GET_NEXT_LINE_H
 
 # include <fcntl.h>
-
-# include <stdio.h>
-
 # include <stdlib.h>
-
-# include <stdlib.h>
-
 # include <unistd.h>
 
 # ifdef BUFFER_SIZE
--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -12,139 +12,117 @@
 
 #include "get_next_line.h"
 
-/*size_t	ft_strlen(char *s)
-{
-	size_t	i;
-
-	i = 0;
-	if (!s)
-	{
-		return (0);
-	}
-	while (s[i])
-	{
-		i++;
-	}
-	return (i);
-}
-
-char	*ft_strchr(const char *str, int c)
+int	found_newline(t_list *list)
 {
 	int	i;
-	int	temp;
 
-	temp = 0;
-	i = 0;
-	while ((char *)str[i] && temp == 0)
-	{
-		if ((char *)str[i] == c)
-		{
-			return ((char *)&str[i]);
-			temp = 1;
-		}
-		i++;
-	}
-	if (c == '\0')
-		return ((char *)&str[i]);
-	return (NULL);
-}
-
-char	*ft_strjoin(char const *s1, char const *s2)
-{
-	int		i;
-	int		len1;
-	int		len2;
-	char	*str;
-
-	if (s1 && s2)
-	{
-		len1 = strlen(s1);
-		len2 = strlen(s2);
-		str = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
-		if (str == '\0')
-			return (NULL);
-		i = -1;
-		while (s1[++i])
-			str[i] = s1[i];
-		while (s2[++i])
-		{
-			str[len1] = s2[i];
-			len1++;
-		}
-		str[len1] = '\0';
-		return (str);
-	}
-	return (NULL);
-}
-
-char	*ft_get_line(char *lonlen)
-{
-	char	*str;
-	int		i;
-
-	i = 0;
-	if (lonlen[i] == '\0')
-		return (NULL);
-	str = (char *)malloc(sizeof(char) * (i + 2));
-	if (!str)
-		return (NULL);
-	i = 0;
-	while (lonlen[i] && lonlen[i])
-	{
-		str[i] = lonlen[i];
-		i++;
-	}
-	if (lonlen[i] == '\n')
-	{
-		str[i] = lonlen[i];
-		i++;
-	}
-	str[i] = NULL;
-	return (str);
-}
-
-void	*ft_izq_line(char *izq_line)
-{
-	int		i;
-	int		j;
-	char	*str;
-
-	i = 0;
-	if (!izq_line[i])
-	{
-		free(izq_line);
-		return (str);
-	}
-	while (izq_line && izq_line)
-		i++;
-	str = (char *)malloc(sizeof(char) * (strlen(izq_line) - i + 1));
-	if (!str)
-		return (NULL);
-	i++;
-	j = 0;
-	while (izq_line[i] != '\0')
-		str[j++] = izq_line[i++];
-	free(izq_line);
-	return (str);
-}*/
-
-int		found_newline(t_list *list)
-{
-	int		i;
 	if (NULL == list)
 		return (0);
 	while (list)
 	{
 		i = 0;
-		while(list ->str_buf[i] && i < BUFFER_SIZE)
+		while (list->str_buf[i] && i < BUFFER_SIZE)
 		{
-			if (list ->str_buf[i])
-			{
-				
-			}
-			
+			if (list->str_buf[i] == '\n')
+				return (1);
+			++i;
 		}
+		list = list->next;
 	}
-	
-	
+	return (0);
+}
+
+t_list	*find_last_node(t_list *list)
+{
+	if (NULL == list)
+		return (NULL);
+	while (list->next)
+		list = list->next;
+	return (list);
+}
+
+/*
+ * Copy (string\n]
+*/
+void	copy_str(t_list *list, char *str)
+{
+	int	i;
+	int	k;
+
+	if (NULL == list)
+		return ;
+	k = 0;
+	while (list)
+	{
+		i = 0;
+		while (list->str_buf[i])
+		{
+			if (list->str_buf[i] == '\n')
+			{
+				str[k++] = '\n';
+				str[k] = '\0';
+				return ;
+			}
+			str[k++] = list->str_buf[i++];
+		}
+		list = list->next;
+	}
+	str[k] = '\0';
+}
+
+/*
+ * find the len to new line in
+ * my linked list
+*/
+int	len_to_newline(t_list *list)
+{
+	int	i;
+	int	len;
+
+	if (NULL == list)
+		return (0);
+	len = 0;
+	while (list)
+	{
+		i = 0;
+		while (list->str_buf[i])
+		{
+			if (list->str_buf[i] == '\n')
+			{
+				++len;
+				return (len);
+			}
+			++i;
+			++len;
+		}
+		list = list->next;
+	}	
+	return (len);
+}
+
+/*
+ * dealloc all from head
+ * set heat->NULL
+*/
+void	dealloc(t_list **list, t_list *clean_node, char *buf)
+{
+	t_list	*tmp;
+
+	if (NULL == *list)
+		return ;
+	while (*list)
+	{
+		tmp = (*list)->next;
+		free((*list)->str_buf);
+		free(*list);
+		*list = tmp;
+	}
+	*list = NULL;
+	if (clean_node->str_buf[0])
+		*list = clean_node;
+	else
+	{
+		free(buf);
+		free(clean_node);
+	}
 }
