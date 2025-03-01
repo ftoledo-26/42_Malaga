fd665fb06db497550d97330239f68e7523bb2b53
ftoledo-26 <franciscomanueltoledo@gmail.com>
2025-03-01 20:08:23 +0100

    Terminado esperando correccion

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -1,2 +1,55 @@
 #include "ft_printf.h"
 
+static int	ft_select_format(va_list argument, const char word);
+
+int	ft_print_character(int character)
+{
+	write(1, &character, 1);
+	return (1);
+}
+
+int	ft_printf(const char *str, ...)
+{
+	int		i;
+	int		size;
+	va_list	argument;
+
+	i = 0;
+	size = 0;
+	va_start(argument, str);
+	while (str[i])
+	{
+		if (str[i] == '%')
+		{
+			size += ft_select_format(argument, str[i + 1]);
+			i++;
+		}
+		else
+		size += ft_printchar(str[i]);
+		i++;
+	}
+	va_end(argument);
+	return (size);
+}
+
+static int	ft_select_format(va_list argument, const char word)
+{
+	int	size;
+
+	size = 0;
+	if (word == 'c')
+		size += ft_printchar(va_arg(argument, int));
+	else if (word == 's')
+		size += ft_printstrin(va_arg(argument, char *));
+	else if (word == 'p')
+		size += ft_printentero(va_arg(argument, unsigned long long));
+	else if (word == 'd' || word == 'i')
+		size += ft_printentero(va_arg(argument, int));
+	else if (word == 'u')
+		size += ft_printunsig(va_arg(argument, unsigned int));
+	else if (word == 'x' || word == 'X')
+		size += ft_print_hexa(va_arg(argument, unsigned int), word);
+	else
+		size += ft_printchar(word);
+	return (size);
+}
--- printf/ft_printf_punteros.c
+++ printf/ft_printf_punteros.c
@@ -1,1 +1,43 @@
 #include "ft_printf.h"
+
+int	ft_print_pointer(unsigned long long ptr)
+{
+	int	size;
+
+	size = 0;
+	size += ft_print_string("0x");
+	if (ptr == 0)
+		size += ft_print_character('0');
+	else
+	{
+		ft_search_pointer(ptr);
+		size += ft_length_pointer(ptr);
+	}
+	return (size);
+}
+
+static int	ft_length_pointer(unsigned long long ptr)
+{
+	int	len;
+
+	len = 0;
+	while (ptr > 0)
+	{
+		len++;
+		ptr /= 16;
+	}
+	return (len);
+}
+
+static void	ft_search_pointer(unsigned long long ptr)
+{
+	if (ptr >= 16)
+		ft_search_pointer(ptr / 16);
+	else
+	{
+		if (ptr < 10)
+            ft_printchar(ptr + '0');
+		else
+            ft_printchar(ptr - 10 + 'a');
+	}
+}
